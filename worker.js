15bdb9d464a54c7ff6b9c7ac4fbb061d0a1f6936
Yuri Makarenko <yuri.makarenko@umbrella-web.com>
2016-03-07 22:06:09 +0300

    Improvement

--- worker.js
+++ worker.js
@@ -12,8 +12,7 @@ tunnel = localtunnel(80, {
   subdomain: subdomain
 }, function(err, tunnel) {
   if (err) {
-  	console.log("Fatal Error: " + err);
-    process.send("Fatal Error: " + err);
+    process.send(err.toString());
     process.exit();
   }
 
