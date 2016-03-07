4ecf79fb10590d60478adbc00e44444da6d62650
Yuri Makarenko <yuri.makarenko@umbrella-web.com>
2016-03-07 22:29:47 +0300

    Improvement

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+process.chdir('/home/wordpress/tunnel');
+
 var cp = require('child_process');
 
 var startTunnel = function(){
-	var child = cp.fork('/home/wordpress/tunnel/worker');
+	var child = cp.fork('./worker');
 
 	child.on('message', function(m) {
 	  // Receive results from child process
