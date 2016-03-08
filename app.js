6c6ef1c4d7c949d0c5d31736aa97f895b2df97d5
Yuri Makarenko <yuri.makarenko@umbrella-web.com>
2016-03-08 20:52:04 +0300

    Improvement

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var args = process.argv.slice(2);
 
 if ( typeof args[0] != 'undefined' && args[0] == 'subdomain' && typeof args[1] != 'undefined' ) {
 	var fs = require('fs');
+	var mysql       = require('mysql');
+	var connection  = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : '',
+	  database : 'wordpress'
+	});
 
 	try {
 		var config = require('./config/default.json');
@@ -9,8 +16,18 @@ if ( typeof args[0] != 'undefined' && args[0] == 'subdomain' && typeof args[1] !
 		var config = {};
 	}
 
+	// save config file
 	config.subdomain = args[1];
 	fs.writeFileSync('./config/default.json', JSON.stringify(config, null, 2) , 'utf-8'); 
+
+	// update parameters in database
+	var domain = config.subdomain + ".localtunnel.me";
+	var httpURL = "http://" + domain;
+	connection.query('UPDATE wordpress.wp_site SET domain = "'+domain+'" WHERE id > 0;');
+    connection.query('UPDATE wp_blogs SET domain = "'+domain+'" WHERE blog_id > 0;');
+    connection.query('UPDATE wp_2_options SET option_value = "'+httpURL+'/stylecaster" WHERE option_name = "siteurl" OR option_name = "home";');
+    connection.query('UPDATE wp_3_options SET option_value = "'+httpURL+'/dailymakeover" WHERE option_name = "siteurl" OR option_name = "home";');
+
 	console.log('Saved subdomain: ' + config.subdomain);
 	return;
 }
